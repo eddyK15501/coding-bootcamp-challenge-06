ca339570c97db4935221342af353a049c7633368
eddyK15501 <edk1231@gmail.com>
2023-07-02 14:31:13 -0400

    playground one with script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,22 @@
+const searchForm = document.querySelector('.search-form')
+const cityName = document.getElementById('city-name');
+// const searchBtn = document.getElementById('search-btn')
+
+let city = ''
+
 
 // WRITTEN WITH .then()
-// const fetchData = () => {
-//     const requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=Raleigh&units=imperial&appid=6a6afa479f8aa91f91f6f65a77189b0f'
-
-//     fetch(requestURL)
-//         .then((res) => res.json())
-//         .then((data) => {
-//             console.log(data)
-//         })
-// }
-
-// const fetchFiveDays = () => {
-//     const requestURL = 'https://api.openweathermap.org/data/2.5/forecast?q=Raleigh&appid=6a6afa479f8aa91f91f6f65a77189b0f';
-
-//     fetch(requestURL)
-//         .then((res) => res.json())
-//         .then((data) => {
-//             console.log(data)
-//         })
-// }
-
-// WRITTEN WITH async await
-const fetchData = async () => {
+const fetchData = () => {
     const requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=Raleigh&units=imperial&appid=6a6afa479f8aa91f91f6f65a77189b0f'
 
-    const response = await fetch(requestURL)
-
-    const data = await response.json()
-
-    console.log(data)
+    fetch(requestURL)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+        })
 }
 
+// WRITTEN WITH async await
 const fetchFiveDays = async () => {
     const requestURL = 'https://api.openweathermap.org/data/2.5/forecast?q=Raleigh&appid=6a6afa479f8aa91f91f6f65a77189b0f'
 
@@ -41,9 +27,15 @@ const fetchFiveDays = async () => {
     console.log(data)
 }
 
-const onSearch = () => {
-    
-}
 
 fetchData()
 fetchFiveDays()
+
+cityName.addEventListener('keypress', (event) => {
+    city = event.target.value
+})
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log(city)
+})
