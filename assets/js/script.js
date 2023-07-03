ecad497f989bca9864e91f8be67d90f1dcfc2e45
eddyK15501 <edk1231@gmail.com>
2023-07-02 23:48:35 -0400

    script.js playground three

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 const searchForm = document.querySelector('.search-form')
 const cityName = document.getElementById('city-name');
-// const searchBtn = document.getElementById('search-btn')
+const searchBtn = document.getElementById('search-btn')
+const section = document.querySelector('section')
 
 let city = ''
-
 
 // WRITTEN WITH .then()
 const fetchData = (cityName) => {
     const requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=6a6afa479f8aa91f91f6f65a77189b0f`
 
     fetch(requestURL)
-        .then((res) => res.json())
+        .then((res) => {
+            if (res.ok) {
+                return res.json()
+            } else {
+                alert('Please enter a valid city')
+            }
+        })
         .then((data) => {
             console.log(data)
         })
@@ -18,7 +24,7 @@ const fetchData = (cityName) => {
 
 // WRITTEN WITH async await
 const fetchFiveDays = async (cityName) => {
-    const requestURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=6a6afa479f8aa91f91f6f65a77189b0f`
+    const requestURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=6a6afa479f8aa91f91f6f65a77189b0f`
 
     const response = await fetch(requestURL)
 
@@ -33,11 +39,16 @@ const fetchFiveDays = async (cityName) => {
 const onFormSubmit = (event) => {
     event.preventDefault()
 
-    fetchData(city)
-    fetchFiveDays(city)
-    
+    city = cityName.value
 
     console.log(city)
+
+    fetchData(city)
+    fetchFiveDays(city)
+
+    cityName.value = ''
+
+    section.classList.remove('hide')
 }
 
 
@@ -47,8 +58,10 @@ const onFormSubmit = (event) => {
 
 
 
-cityName.addEventListener('keypress', (event) => {
-    city = event.target.value
-})
+// cityName.addEventListener('keypress', (event) => {
+//     city = event.target.value
+// })
+
+// searchBtn.addEventListener('click', onFormSubmit)
 
 searchForm.addEventListener('submit', onFormSubmit)
